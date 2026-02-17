55edc7c58358f1e1d3df66f61a912f8a00209b36
Julia Rosa <Julia.rosalar06@gmal.com>
2026-02-16 21:31:58 -0300

    feat: completed static ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,137 @@
-const Home = () => {
-  return (
-    <div>Julia</div>
-  )
-}
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Card, CardHeader, CardContent } from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
+import {
+	Plus,
+	List,
+	Check,
+	PinOff,
+	SquarePen,
+	Trash,
+	ListChecks,
+	Sigma,
+} from "lucide-react";
 
-export default Home
+import { Badge } from "@/components/ui/badge";
+import {
+	AlertDialog,
+	AlertDialogAction,
+	AlertDialogCancel,
+	AlertDialogContent,
+	AlertDialogFooter,
+	AlertDialogHeader,
+	AlertDialogTitle,
+	AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+
+import {
+	Dialog,
+	DialogContent,
+	DialogHeader,
+	DialogTitle,
+	DialogTrigger,
+} from "@/components/ui/dialog";
+
+const Home = () => {
+	return (
+		<main className="w-full h-screen bg-gray-100 flex justify-center items-center">
+			<Card className="max-w-lg w-full p-4">
+				<CardHeader className="flex gap-2">
+					<Input placeholder="Adicionar tarefa" />
+					<Button variant="default" className="cursor-pointer">
+						<Plus />
+						Cadastrar
+					</Button>
+				</CardHeader>
+
+				<CardContent>
+					<Separator className="mb-4" />
+
+					<div className="flex gap-2">
+						<Badge className="cursor-pointer" variant="default">
+							<List />
+							Todas
+						</Badge>
+						<Badge className="cursor-pointer" variant="outline">
+							<PinOff />
+							Não finalizadas
+						</Badge>
+						<Badge className="cursor-pointer" variant="outline">
+							<Check />
+							Concluídas
+						</Badge>
+					</div>
+
+					<div className="mt-4 border-b">
+						<div className="h-14 flex justify-between items-center border-b border-t">
+							<div className="w-1 h-full bg-green-300"></div>
+							<p className="flex-1 px-2 text-sm">Estudar React</p>
+							<div className="flex gap-2 items-center">
+								<Dialog>
+									<DialogTrigger asChild>
+										<SquarePen
+											size={16}
+											className="cursor-pointer bg-gray-100"
+										/>
+									</DialogTrigger>
+									<DialogContent>
+										<DialogHeader>
+											<DialogTitle>Editar tarefa</DialogTitle>
+										</DialogHeader>
+
+										<div className="flex gap-2">
+											<Input placeholder="Editar tarefa" />
+											<Button className="cursor-pointer">Salvar</Button>
+										</div>
+									</DialogContent>
+								</Dialog>
+								<Trash size={16} className="cursor-pointer bg-gray-100" />
+							</div>
+						</div>
+					</div>
+
+					<div className="flex justify-between mt-4">
+						<div className="flex gap-2 items-center">
+							<ListChecks size={18} />
+							<p className="text-xs">Tarefas concluídas (3/3)</p>
+						</div>
+						<AlertDialog>
+							<AlertDialogTrigger asChild>
+								<Button
+									className="text-xs h-7 cursor-pointer"
+									variant="outline">
+									<Trash />
+									Limpar tarefas concluídas
+								</Button>
+							</AlertDialogTrigger>
+							<AlertDialogContent>
+								<AlertDialogHeader>
+									<AlertDialogTitle>
+										Tem certeza que deseja excluir x itens?
+									</AlertDialogTitle>
+								</AlertDialogHeader>
+								<AlertDialogFooter>
+									<AlertDialogAction>Sim</AlertDialogAction>
+									<AlertDialogCancel>Cancelar</AlertDialogCancel>
+								</AlertDialogFooter>
+							</AlertDialogContent>
+						</AlertDialog>
+					</div>
+					<div className="h-2 w-full bg-gray-100  mt-4 rounded-md">
+						<div
+							className="h-full bg-violet-500 rounded-md"
+							style={{ width: "50%" }}></div>
+					</div>
+
+					<div className="flex justify-end items-center mt-2 gap-2">
+						<Sigma size={18} />
+						<p className="text-xs">3 tarefas no total</p>
+					</div>
+				</CardContent>
+			</Card>
+		</main>
+	);
+};
+
+export default Home;
